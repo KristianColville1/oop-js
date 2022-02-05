ec51f62eb0947aa6cd022c241a9c22cc34d3c573
KristianColville1 <kristiancolville1@gmail.com>
2022-02-05 10:51:26 +0000

    Add check for if block is in the game window

--- assets/js/games.js
+++ assets/js/games.js
@@ -12,9 +12,21 @@ function jump(){
 var checkIfDead = setInterval(function(){
     var charactersTopPos = parseInt(window.getComputedStyle(character).getPropertyValue('top'));
     var blockLeftPos = parseInt(window.getComputedStyle(block).getPropertyValue('left'));
-    if(blockLeftPos < 180 && blockLeftPos > 100 && charactersTopPos >= 470){
+    if(blockLeftPos < 185 && blockLeftPos > 100 && charactersTopPos >= 500){
         block.style.animation = 'none';
         block.style.display = 'none';
         alert('You lose ha ha ha..');
+        
     }
 }, 10);
+
+var isInWindow = setInterval(function(){
+    blockLeftPos = parseInt(window.getComputedStyle(block).getPropertyValue('left'));
+    if(blockLeftPos <= 100){
+        block.style.display = 'none';
+    }
+    if(blockLeftPos == 1150){
+        block.style.display = 'block';
+    }
+
+}, 5);
