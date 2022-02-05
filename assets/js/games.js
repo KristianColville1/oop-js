3a188358e3a344b20f6105ff7416a9a0da46f87e
KristianColville1 <kristiancolville1@gmail.com>
2022-02-05 10:14:43 +0000

    Add collision detection function to check if player dead

--- assets/js/games.js
+++ assets/js/games.js
@@ -1,8 +1,20 @@
-var character = document.getElementById("character");
-var block = document.getElementById("block");
+var character = document.getElementById('character');
+var block = document.getElementById('block');
 function jump(){
-    character.classList.add("animate-c");
+    if(character.classList !== 'animate-c'){
+        character.classList.add('animate-c');
+    }
     setTimeout(function(){
-        character.classList.remove("animate-c");
+        character.classList.remove('animate-c');
     }, 1000);
 }
+
+var checkIfDead = setInterval(function(){
+    var charactersTopPos = parseInt(window.getComputedStyle(character).getPropertyValue('top'));
+    var blockLeftPos = parseInt(window.getComputedStyle(block).getPropertyValue('left'));
+    if(blockLeftPos < 180 && blockLeftPos > 100 && charactersTopPos >= 470){
+        block.style.animation = 'none';
+        block.style.display = 'none';
+        alert('You lose ha ha ha..');
+    }
+}, 10);
