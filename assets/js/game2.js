4ca16eee6f56ddf8998bd72cea84872778d1b851
Kristian Philip Colville <kristiancolville1@gmail.com>
2022-02-10 21:36:32 +0000

    Testing begins, structure ready and styled

---
+++ assets/js/game2.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    // need to create suduko logic for generating boards for the player;
+    // we have 9 red cubes each holding 9 black cubes
+    // every red cube needs 1-9 and every black cube needs to equal 1-9 horizontal and vertical
+
+    // how can I layer elements and cheat in not creating over complicated logic?
+
+    // I need to create a new game board each time for the player and have different difficulty levels for
+
+    // Solution One: create a minimal viable product and then work out the kinks
+    // Solution Two: watch tutorials like an idiot
+    // Solution Three: Code it ffs
+
+    
+})
