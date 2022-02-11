c4b0ff27a39df58df7045945191fb0028c55dea8
Kristian Philip Colville <kristiancolville1@gmail.com>
2022-02-11 08:38:49 +0000

    Create more game logic to work with

--- assets/js/game2.js
+++ assets/js/game2.js
@@ -17,11 +17,11 @@ document.addEventListener("DOMContentLoaded", () => {
     // Game board:
     // 9 red cubes
     // 81 black cubes
-    // needs to change color when hovered over by user
+    // needs to change color when hovered over by user.....................................
 
     // How the hell can you do this without knowing math..
     // okay so we have the ability to create arrays and dictionaries...
-    // It might be easier to put the black cubes inside the red cubes creating a multidimensional array?
+    // It might be easier to put the black cubes inside the red cubes creating a multidimensional array maybe?
 
     // width <- horizonal, height <- vertical
 
@@ -41,6 +41,9 @@ document.addEventListener("DOMContentLoaded", () => {
     const lastIndex = 0;
     const length = 9;
     const width = 9;
+
+    // after testing numbers 1 - 9 into red cubes we can randomise the numbers between 1 & 9 and
+    let randomNum = Math.floor(Math.random() * 9);
     let divText = 1;
     const totalPositions = [ 0,
         1, 2, 3, 4, 5, 6, 7, 8,
@@ -64,29 +67,44 @@ document.addEventListener("DOMContentLoaded", () => {
         // blackCubesArray.forEach(index => blackCubes[index].classList.remove('number'));
 
         // need to create 1-9 numbers inside 1-9 red cubes
-
-        // for 81 positions every 9 positions reset divText to 1 
-        for(let num = 0; num < totalPositions.length; num++){
-
-            blackCubes[num].innerText = divText++;
-            if(divText > 9){
-                divText = 1;
-            }
-        }
-
         // I have created a for loop which inputs the numbers 1 - 9 into each red cube.
 
         // now I need to randomise the numbers and then make sure they increment properly in each direction of the array.
         // we know its possible because the game exists.
 
         // firstly lets make div text random between 1 - 9
+        // for 81 positions every 9 positions reset divText to 1 
+        // for(let num = 0; num < totalPositions.length; num++){
+        //     randomNum = Math.floor(Math.random() * 9 + 1);
+        //     if(randomNum > 9){
+        //         randomNum = 9;
+        //     } else if(randomNum < 1){
+        //         randomNum = 1;
+        //     }
 
+        //     blackCubes[num].innerText = randomNum;// divText++;
 
+        // }
 
-        
-        
+        for(let num = 0; num < totalPositions.length; num++){
+            randomNum = Math.floor(Math.random() * 9 + 1);
+            if(randomNum > 9){
+                randomNum = 9;
+            } else if(randomNum < 1){
+                randomNum = 1;
+            }
+
+            blackCubes[num].innerText = randomNum;// divText++;
+
+        }
 
     }
+
+    // current tasks:
+
+    // array 1: Full suduko board numbers 1 - 81, the text within the elements.
+    // create a second 81 element array which stores the contents of the orginal array for comparison.
+    // remove random number value and use array and each time random index picked remove that index and then reset the array for each red cube
 
     startButton.addEventListener('click', runGame);
 });
